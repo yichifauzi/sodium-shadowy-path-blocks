65388427c9e1204e52fc00905e46fedb95f19f73
Rynnavinx <35942714+Rynnavinx@users.noreply.github.com>
2024-08-10 01:42:49 -0700

    Change the method that modifies Indium AO mode for path blocks to use ModifyVariable instead of Redirect

--- src/main/java/rynnavinx/sspb/mixin/indium/MixinAoCalculator.java
+++ src/main/java/rynnavinx/sspb/mixin/indium/MixinAoCalculator.java
@@ -1,17 +1,14 @@
 package rynnavinx.sspb.mixin.indium;
 
-import link.infra.indium.Indium;
 import link.infra.indium.renderer.aocalc.AoCalculator;
 import link.infra.indium.renderer.aocalc.AoConfig;
 import link.infra.indium.renderer.render.BlockRenderInfo;
 
 import net.minecraft.block.DirtPathBlock;
 
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import rynnavinx.sspb.client.SSPBClientMod;
 
@@ -42,13 +39,11 @@ public abstract class MixinAoCalculator {
         return sspb$modifyW1(w1);
     }
 
-    @Redirect(method = "compute", at = @At(value = "FIELD", target = "Llink/infra/indium/Indium;AMBIENT_OCCLUSION_MODE:Llink/infra/indium/renderer/aocalc/AoConfig;", opcode = Opcodes.GETSTATIC), remap = false)
-    private AoConfig redirectAoMode(){
+    @ModifyVariable(method = "compute", at = @At("STORE"), ordinal = 0, remap = false)
+    private AoConfig modifyAoConfig(AoConfig config){ // IntelliJ complains, but it works as expected
         if(SSPBClientMod.options().vanillaPathBlockLighting && blockInfo.blockState.getBlock() instanceof DirtPathBlock){
             return AoConfig.VANILLA;
         }
-        else{
-            return Indium.AMBIENT_OCCLUSION_MODE;
-        }
+        return config;
     }
 }
